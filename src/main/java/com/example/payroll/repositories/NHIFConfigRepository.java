b7c2862fe22c3b554358165f1c4e59e1213855c6
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-15 14:17:54 +0300

    NHIFConfigRepository

--- src/main/java/com/example/payroll/repositories/NHIFConfigRepository.java
+++ src/main/java/com/example/payroll/repositories/NHIFConfigRepository.java
@@ -1,5 +1,9 @@
 package com.example.payroll.repositories;
 
-public interface NHIFConfigRepository {
+import com.example.payroll.model.NHIFConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface NHIFConfigRepository extends JpaRepository<NHIFConfig, Long>{
 }
