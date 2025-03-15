c7e2034037f940f545b1c0349d734a5df22db27f
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-15 11:46:41 +0300

    TaxBandRepository

--- src/main/java/com/example/payroll/repositories/TaxBandRepository.java
+++ src/main/java/com/example/payroll/repositories/TaxBandRepository.java
@@ -1,5 +1,10 @@
 package com.example.payroll.repositories;
 
-public interface TaxBandRepository {
+import com.example.payroll.model.TaxBand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaxBandRepository extends JpaRepository<TaxBand, Long>{
 
 }
