508ea132456bd2bdf03bbd0e9b0f1a8f9cb56435
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-15 14:21:05 +0300

    NSSFConfigRepository

--- src/main/java/com/example/payroll/repositories/NSSFConfigRepository.java
+++ src/main/java/com/example/payroll/repositories/NSSFConfigRepository.java
@@ -1,5 +1,9 @@
 package com.example.payroll.repositories;
 
-public interface NSSFConfigRepository {
+import com.example.payroll.model.NSSFConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface NSSFConfigRepository extends JpaRepository<NSSFConfig, Long> {
 }
