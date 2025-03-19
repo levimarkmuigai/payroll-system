438c9000f549c8b152da55a9a95cf4d3e439c264
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-19 20:21:40 +0300

    EmployeeServicePAYECalc

--- src/main/java/com/example/payroll/service/EmployeeService.java
+++ src/main/java/com/example/payroll/service/EmployeeService.java
@@ -18,10 +18,20 @@ import java.util.Optional;
 public class EmployeeService {
     // Injecting the Repository to interact with the Database
     private final EmployeeRepository employeeRepository;
+    private final NHIFConfigRepository nhifConfigRepository;
+    private final NSSFConfigRepository nssfConfigRepository;
+    private final TaxBandRepository taxBandRepository;
+
 
     @Autowired // Construtor injection for the repository
-    public EmployeeService(EmployeeRepository employeeRepository){
+    public EmployeeService(EmployeeRepository employeeRepository
+            , NHIFConfigRepository nhifConfigRepository
+            , NSSFConfigRepository nssfConfigRepository
+            , TaxBandRepository taxBandRepository) {
         this.employeeRepository = employeeRepository;
+        this.nhifConfigRepository = nhifConfigRepository;
+        this.nssfConfigRepository = nssfConfigRepository;
+        this.taxBandRepository = taxBandRepository;
     }
 
     // Method to retrieve the Employees from the database
@@ -42,5 +52,29 @@ public class EmployeeService {
     // Method to delete an employee by id
     public void deleteEmployee(Long id){
         employeeRepository.deleteById(id);
+    }
+
+    /**
+     * Calaculate the PAYE (income tax) based on the employee's basic salary and tax bands
+     * 
+     * @param employee the Employee whose tax is to be calculated
+     * @return the calculated PAYE amount
+    */
+    public  double calaculatePAYE(Employee employee){
+        double basicSalary = employee.getBasicSalary();
+        List<TaxBand> taxBands = taxBandRepository.findAll();
+        double taxDue = 0;
+
+        // Sort the tax bands by lower limit
+        taxBands.sort((a, b) -> Double.compare(a.getlowerLimit(), b.getlowerLimit()));
+
+        double remainingSalary = basicSalary;
+        for(TaxBand band : taxBands){
+            if(remainingSalary <= 0 )break;
+            double taxableInBand = Math.min(remainingSalary, band.getupperLimit() - band.getlowerLimit());
+            taxDue += taxableInBand * (band.gettaxRate() / 100);
+            remainingSalary -= taxableInBand;
+        }
+        return taxDue;
     }
 }
