bf1fcc1f6fe77dbaf99f97c3785cc6dd46ec3cc0
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-21 10:40:13 +0300

    PayrollSummaryDTO

--- src/main/java/com/example/payroll/model/PayrollSummary.java
+++ src/main/java/com/example/payroll/model/PayrollSummary.java
@@ -1,5 +1,17 @@
 package com.example.payroll.model;
 
-public class PayrollSummary {
 
-}
+/**
+ * Represents a computed payrol summary for a given employee.
+ * This is a DTO (Data Transfer Object) class used to return  calculated payroll data.
+ */
+public record PayrollSummary(
+    Long employeeId,
+    String employeeName,
+    double basicSalary,
+    double grossSalary,
+    double nhifDeduction,
+    double nssfDeduction,
+    double paye,
+    double AllowanceConfig
+){}
